d300eeb7122b9e5ad8c33e1f49c04654d1dafff3
Waldston Santana <waldston.ws@gmail.com>
2023-12-21 11:09:37 -0300

    Finished Project

---
+++ model/sevices/BrazilTaxService.java
@@ -0,0 +1,10 @@
+package model.sevices;
+
+public class BrazilTaxService {
+    public  double tax(double amount) {
+        if (amount <= 100.0) {
+            return amount * 0.20;
+        }
+        else return  amount * 0.15;
+    }
+}
---
+++ model/sevices/RentalService.java
@@ -0,0 +1,37 @@
+package model.sevices;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+import java.time.Duration;
+
+public class RentalService {
+    private Double pricePerDay;
+    private Double pricePErHour;
+    private  BrazilTaxService taxService;
+
+    public RentalService(Double pricePerDay, Double pricePErHour, BrazilTaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePErHour = pricePErHour;
+        this.taxService = taxService;
+    }
+
+    public void processInVoice(CarRental carRental) {
+       double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+       double hours = minutes / 60;
+
+       double basicPayment;
+       if (hours <= 12.0 ) {
+           basicPayment = pricePErHour * Math.ceil(hours);
+
+       }
+       else {
+           basicPayment = pricePerDay * Math.ceil(hours / 24);
+       }
+
+       double tax = taxService.tax(basicPayment);
+       carRental.setInvoice(new Invoice(basicPayment,tax));
+
+
+    }
+}
